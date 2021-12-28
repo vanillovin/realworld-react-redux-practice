551d9d14ccaa5b9299a297e0df849ab23f8353ef
vanillovin <vanillovin@gmail.com>
2021-12-28 09:34:42 +0900

    react query 를 이용해서 articleList data fetch

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,16 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Home from './realworld/pages/Home';
 import './style.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <Home />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/realworld/pages/Home.tsx
+++ src/realworld/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import NavBar from '../../components/NavBar';
 import ArticlePreview from '../../components/ArticlePreview';
+import { useQuery } from 'react-query';
 
 // type vs interface (넓은 개념)
 // (컴포넌트와 컴포넌트 사이의 props)
@@ -20,43 +21,19 @@ type Article = {
   tagList: string[];
 };
 
-// apiArticleList 분리
+//
+// refactor: useQuery로 articleList 상태 관리
 
 // 추상화... 코드를 사람처럼 생각 (주방장과 손님) 주방장 함수 ><ㅡㅏ>?
 // 코드 쪼개기의 원칙. 서로 연결된 로직들은 응집력
 // 내부에 있지 않아도 되는 건 밖에 안에있는 게 좋은 경우도 있음.
 function Home() {
-  // https://react-redux.realworld.io/#/?_k=z6qyc9
-  const [articleList, setArticleList] = useState<Article[] | undefined>(
-    undefined
+  // https://react-query.tanstack.com/overview
+  const { isLoading, error, data } = useQuery<Article[]>('Articles', () =>
+    fetch('https://api.realworld.io/api/articles?limit=10&offset=0').then(
+      (res) => res.json()
+    )
   );
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(undefined);
-  // (REACT HOOK 작동 방식 공부)
-  // dependency array 의존성 배열
-  // https://velog.io/@velopert/react-hooks#2-useeffect
-
-  function fetchArticles() {
-    setLoading(true);
-    setLoading(undefined);
-
-    fetch(`https://api.realworld.io/api/articles?limit=10&offset=0`)
-      .then((res) => res.json())
-      .then((body) => setArticleList(body.articles))
-      .catch(setError)
-      .finally(() => {
-        setLoading(false);
-      });
-  }
-
-  useEffect(() => {
-    // couter 예시 연습, error refetch
-    // 처음 mount 될 때
-    // 의존성 배열에 있는 상태가 업데이트될 때~
-    fetchArticles();
-  }, [setArticleList]);
-
-  // 매 렌더링마다 다시 계산하거나, 호출하고 싶으면... useEffect 밖에!
 
   return (
     <div id="main">
@@ -90,8 +67,8 @@ function Home() {
                     다시 요청하기
                   </button>
                   {error?.message}
-                  {loading && '로딩중'}
-                  {articleList?.map((article) => (
+                  {isLoading && '로딩중'}
+                  {data?.map((article) => (
                     <ArticlePreview key={article.slug} {...article} />
                   ))}
                 </div>
